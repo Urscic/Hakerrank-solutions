60237d99b1118d1ef2e4f75d6591f077327070eb
Urscic <83347321+Urscic@users.noreply.github.com>
2022-10-11 21:45:48 +0200

    Update minion_game.py

--- minion_game.py
+++ minion_game.py
@@ -1,40 +1,45 @@
-# Stuart B
-from itertools import count
-from operator import index
-import string
-word = 'banana'
-consonants = list(string.ascii_lowercase)
-vowels = ['a', 'e', 'i', 'o', 'u']
-for i in range(len(vowels)):
-    if vowels[i] in consonants:
-        consonants.remove(vowels[i])
-stuart = []
-for i in range(len(word)):
-    if word[i] in consonants:
-        index = i
-        stuart.append(word[index:])
-        # bez -1 ostanie słowo będzie puste
-        for j in range(len(word[index:])-1):
-            new_word = word[index:(-1-j)]
-            stuart.append(new_word)
+def minion_game(word):
+    from itertools import count
+    from operator import index
+    import string
+    consonants = list(string.ascii_lowercase)
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    stuart = []
+    kevin = []
+    for i in range(len(vowels)):
+        if vowels[i] in consonants:
+            consonants.remove(vowels[i])
+    
+    for i in range(len(word)):
+        if word[i] in consonants:
+            index = i
+            stuart.append(word[index:])
+            for j in range(len(word[index:])-1):
+                new_word = word[index:(-1-j)]
+                stuart.append(new_word)
 
-print(stuart)
-kevin = []
-# Kewvin A
-for i in range(len(word)):
-    if word[i] in vowels:
-        index = i
-        kevin.append(word[index:])
-        for j in range(len(word[index:])-1):
-            new_word = word[index:(-1-j)]
-            kevin.append(new_word)
-print(kevin)
-if len(kevin) > len(stuart):
-    print("Kevin", len(kevin))
+    
+    for i in range(len(word)):
+        if word[i] in vowels:
+            index = i
+            kevin.append(word[index:])
+            for j in range(len(word[index:])-1):
+                new_word = word[index:(-1-j)]
+                kevin.append(new_word)
+    if len(kevin) > len(stuart):
+        print("Kevin", len(kevin))
 
-elif len(stuart)> len(kevin):
-    print("Stuart", len(stuart))
+    elif len(stuart)> len(kevin):
+        print("Stuart", len(stuart))
 
-else: print('draw')
+    elif len(kevin)==len(stuart): 
+        print('Draw')
+    
+    print(stuart)
+    print(kevin)
+      
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
 
 
